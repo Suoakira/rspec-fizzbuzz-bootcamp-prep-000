df6a107e72c6415a88be86f17fb6178c37035dc2
Stephen James <steves0089@gmail.com>
2018-08-30 13:50:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 
 def fizzbuzz(x)
-  if x % 3 == 0 
+  if x % 3 == 0 && x % 5 == 0
+    "FizzBuzz"
+  elsif x % 3 == 0 
     "Fizz"
   elsif x % 5 == 0
     "Buzz"
-  elsif x % 3 == 0 && x % 5 == 0
-    "FizzBuzz"
   end
 end
